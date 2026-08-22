9255e0fe243114362726df308038dfbbde11722b
Jon Cihlar <36781511+jonathoncihlar@users.noreply.github.com>
2026-08-22 15:50:08 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Coding 1 Hello World
+In this code along activity, you will follow your teacher to write a program that says "Hello World!" on the console.
---
+++ hello_world.py
@@ -0,0 +1,14 @@
+"""
+This program will print out Hello World
+Name - Date
+"""
+
+
+def main() -> None:
+    pass
+
+
+
+# don't change anything below this line
+if __name__ == "__main__":
+    main()
