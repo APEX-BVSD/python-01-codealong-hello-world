b5529df91b4e9447e25017381bebb9d2e51acab4
Jon Cihlar <36781511+jonathoncihlar@users.noreply.github.com>
2026-08-23 15:47:35 -0600

    Revise header comment.

--- hello_world.py
+++ hello_world.py
@@ -1,7 +1,7 @@
 """
 This is a first program to output Hello World to the console. It will then
 be enhanced to ask the user for their name and greet them by name.
-Name - Month Year
+First Last - Month Year
 """
 
 def main() -> None:
